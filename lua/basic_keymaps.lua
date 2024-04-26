4ad56b0342361924cf1c58df915def1403087b9e
Miron <mironnesvetaylov@gmail.com>
2024-04-26 11:34:00 +0300

    Nvim-tree float mode!

--- lua/basic_keymaps.lua
+++ lua/basic_keymaps.lua
@@ -4,8 +4,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>le', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 
 local api = vim.api
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,7 @@
+-- https://www.reddit.com/r/neovim/comments/wvcz64/nvimtreelua_how_to_center_floating_window/
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.6  -- You can change this too
+
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
@@ -16,12 +20,40 @@ return {
       sort = {
         sorter = "case_sensitive",
       },
-      view = {
-        width = 30,
-      },
+
+  view = {
+    relativenumber = true,
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = "rounded",
+          relative = "editor",
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
+  },
+
       renderer = {
         group_empty = true,
       },
+
       filters = {
         enable = true,
         git_ignored = true,
