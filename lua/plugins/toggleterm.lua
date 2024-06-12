e797c6ca8d93e5f638f41bfdc04e509859764cae
miron2363 <mironnesvetaylov@gmal.com>
2024-06-12 17:06:29 +0300

    Do not set power shell if it is wsl. Add csharp lsp

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,26 +3,24 @@ return {
   { 'akinsho/toggleterm.nvim', version = "*",
   event = "VeryLazy",
     config = function()
-      local _shell = ""
 
-      if vim.fn.executable("pwsh") then
-        _shell = "pwsh"
-      else
-        _shell = "bash"
+
+      if vim.fn.has("win32") and not vim.fn.has("wsl") then
+        local powershell_options = {
+          shell = "pwsh",
+          shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+          shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+          shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+          shellquote = "",
+          shellxquote = "",
+        }
+
+        for option, value in pairs(powershell_options) do
+          vim.opt[option] = value
+        end
       end
 
-      local powershell_options = {
-        shell = _shell,
-        shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-        shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-        shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-        shellquote = "",
-        shellxquote = "",
-      }
 
-      for option, value in pairs(powershell_options) do
-        vim.opt[option] = value
-      end
 
       require("toggleterm").setup({
         open_mapping = "<c-t>",
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,6 +16,7 @@ return {
         "vim",
         "vimdoc",
         "go",
+        "c_sharp",
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
