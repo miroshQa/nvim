3fab08e75cd87d3b6a307c0f66a00803b60397d5
miron2363 <mironnesvetaylov@gmal.com>
2024-06-15 15:31:30 +0300

    Simplify autopairs and cmp config. Delete neo-tree image dependency

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -2,31 +2,8 @@ return  {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
     config = function()
-
-      local status_ok, npairs = pcall(require, "nvim-autopairs")
-
-      npairs.setup {
+      require("nvim-autopairs").setup {
         check_ts = true,
-        disable_filetype = { "TelescopePrompt", "spectre_panel" },
-        fast_wrap = {
-          map = "<M-e>",
-          chars = { "{", "[", "(", '"', "'" },
-          pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-          offset = 0, -- Offset from pattern match
-          end_key = "$",
-          keys = "qwertyuiopzxcvbnmasdfghjkl",
-          check_comma = true,
-          highlight = "PmenuSel",
-          highlight_grey = "LineNr",
-        },
       }
-
---    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
---    local cmp_status_ok, cmp = pcall(require, "cmp")
---    if not cmp_status_ok then
---      return
---    end
---    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
-
   end,
   }
