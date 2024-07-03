855c03df39afe4bdd47b29951f0caa9c5e1ad086
Miron <mironnesvetaylov@gmail.com>
2024-07-03 16:40:22 +0300

    Add proper highlight for search (/ ?). Add blue label color for flash nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,31 @@
 return {
 	{
 		"luisiacc/gruvbox-baby",
+    enable = false,
 		priority = 1000,
 		config = function()
 			vim.g.gruvbox_baby_use_original_palette = true
+			vim.g.gruvbox_baby_color_overrides = {}
+
+      -- See help IncSearch / CurSearch
+
+      vim.g.gruvbox_baby_highlights = {
+        IncSearch = { bg = "#ff9e64", fg = "White" },
+        CurSearch = {link = "IncSearch"},
+        FlashLabel = {bg = "#0076CE"}
+      }
 
 			require("lualine").setup({
 				options = {
 					theme = "gruvbox-baby",
 				},
 			})
+
 			vim.cmd.colorscheme("gruvbox-baby")
 		end,
 	},
+
+  {
+    "sainnhe/gruvbox-material"
+  }
 }
