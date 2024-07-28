a399872841c17f5aaee3cebcbc424eff12717c16
Miron <mironnesvetaylov@gmail.com>
2024-07-28 11:54:23 +0300

    Add macro notification for noice

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,6 +4,29 @@ vim.keymap.set('',  '<Esc>', "<ESC>:noh<CR>:lua require('notify').dismiss()<CR>"
 -- It also adds search labels for / and ?, *, #, n N, , so I can delete hlslens
 -- That is such a amazing plugin!!! I should't never delete it!
 
+vim.api.nvim_create_autocmd("RecordingEnter", {
+  callback = function()
+    local msg = string.format("Register:  %s", vim.fn.reg_recording())
+    _MACRO_RECORDING_STATUS = true
+    vim.notify(msg, vim.log.levels.INFO, {
+      title = "Macro Recording",
+      keep = function() return _MACRO_RECORDING_STATUS end,
+    })
+  end,
+  group = vim.api.nvim_create_augroup("NoiceMacroNotfication", {clear = true})
+})
+
+vim.api.nvim_create_autocmd("RecordingLeave", {
+  callback = function()
+    _MACRO_RECORDING_STATUS = false
+    vim.notify("Success!", vim.log.levels.INFO, {
+      title = "Macro Recording End",
+      timeout = 2000,
+    })
+  end,
+  group = vim.api.nvim_create_augroup("NoiceMacroNotficationDismiss", {clear = true})
+})
+
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
