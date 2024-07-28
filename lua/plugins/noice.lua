317b043c7a23d5e83e045e2d2596cd11655d0199
Miron <mironnesvetaylov@gmail.com>
2024-07-28 12:45:33 +0300

    Hide 'wrriten' notifications

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -46,6 +46,18 @@ return {
         long_message_to_split = true, -- long messages will be sent to a split
         lsp_doc_border = true, -- add a border to hover docs and signature help
       },
+      -- https://github.com/folke/noice.nvim/wiki/Configuration-Recipes
+      -- Hide written messages
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            kind = "",
+            find = "written",
+          },
+          opts = { skip = true },
+        },
+      },
     })
   end,
 }
