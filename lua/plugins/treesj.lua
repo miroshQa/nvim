be5701b4988c8d8036c1169092403f324d45d245
Miron <mironnesvetaylov@gmail.com>
2024-07-29 12:57:25 +0300

    Add treejs. (It is just crazy game changer for unflatting lists / tables / function calls)

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,15 @@
+vim.keymap.set("n", "gs", "<cmd>lua require('treesj').toggle()<CR>", {desc = "Toggle split / join"})
+vim.keymap.set("n", "gS", "<cmd>lua require('treesj').toggle({split = {recursive = true}})<CR>", {desc = "Toggle split / join recursively"})
+
+return {
+  'Wansmer/treesj',
+  lazy = true,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = function()
+    require('treesj').setup({
+      max_join_length = 250,
+    })
+  end,
+}
