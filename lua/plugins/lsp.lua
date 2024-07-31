4ce16006ae9485c861c1871dd887d6ad0a9606ca
Miron <mironnesvetaylov@gmail.com>
2024-07-31 11:31:36 +0300

    Ignore useless symbols lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,9 +16,10 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set("n", "M", vim.diagnostic.open_float, {desc = "Misstake hover (Open Error / Diagnostic float)"})
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {desc = "Hover Signature / Documentation"})
 
+local adequate_symbols = {"function", "class", "struct", "method", "enum", "interface", "type"}
 vim.keymap.set("n", "<leader>w", "<cmd>Telescope diagnostics<cr>", { desc = "Find Workspace Diagnostics" })
-vim.keymap.set("n", "<leader>s", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>", { desc = 'Find symbols'})
-vim.keymap.set("n", "<leader>S", "<cmd>lua require('telescope.builtin').lsp_workspace_symbols()<CR>", { desc = "Find symbols in workspace" })
+vim.keymap.set("n", "<leader>s", function() require('telescope.builtin').lsp_document_symbols({symbols = adequate_symbols}) end, { desc = 'Find symbols'})
+vim.keymap.set("n", "<leader>S", function() require('telescope.builtin').lsp_workspace_symbols({symbols = adequate_symbols}) end, { desc = "Find symbols in workspace" })
 vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, {desc = "Lsp actions"})
 vim.keymap.set("n", "<leader>ua", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, {desc = "Toggle inlay_hints (Annotations)"})
 vim.keymap.set("n", "<leader>um", "<cmd>Mason<CR>", {desc = "Open Mason"})
