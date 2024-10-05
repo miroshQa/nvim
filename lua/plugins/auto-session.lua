698fc21c9884ac2edce468304047b81086314892
Miron <mironnesvetaylov@gmail.com>
2024-10-05 20:00:35 +0300

    style: Pretify my telescope config

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -4,11 +4,13 @@ return {
   'rmagatti/auto-session',
   lazy = false,
 
-  ---enables autocomplete for opts
   ---@module "auto-session"
   ---@type AutoSession.Config
   opts = {
     suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-    -- log_level = 'debug',
+    -- https://github.com/rmagatti/auto-session/issues/244
+    session_lens = {
+      load_on_setup = false,
+    }
   },
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,53 +1,32 @@
-
-vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
-vim.keymap.set("n", "<leader>/", "<CMD>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", {desc = "Search for cwd"})
-vim.keymap.set("n", "<leader>'", "<cmd>Telescope resume<cr>", { desc = 'Search resume' })
-vim.keymap.set("n", "<leader>:", "<cmd>Telescope command_history<cr>", { desc = 'Search command history' })
-vim.keymap.set("n", "<leader>sp", "<cmd>Telescope builtin<cr>", { desc = "Search telescope picker" })
-vim.keymap.set("n", "<leader>b", "<cmd>Telescope git_branches<cr>", { desc = "Search branches" })
-vim.keymap.set("n", "<leader>se", function () require("telescope.builtin").symbols({sources = {"emoji", "gitmoji"}}) end, { desc = "Search emoji" })
-vim.keymap.set("n", "<leader>sn", "<cmd>Telescope notify<cr>", { desc = "Search notification" })
-vim.keymap.set("n", "<leader>.", "<cmd>Telescope oldfiles<cr>", { desc = 'Search old files ("." for repeat)' })
-vim.keymap.set("n", "<leader>k", "<cmd>Telescope help_tags<cr>", { desc = 'Search help tags' })
-vim.keymap.set("n", "<leader>c", "<cmd>Telescope git_bcommits<cr>", { desc = 'Search commits for current buffer' })
-vim.keymap.set("n", "<leader>C", "<cmd>Telescope git_commits<cr>", { desc = 'Search commits' })
-vim.keymap.set("n", "<leader>g", function() require('telescope.builtin').git_status({path_display = {'tail'}}) end, { desc = 'Search edited / added files' })
-
-
-vim.keymap.set("n", "<leader>sc", function () require("telescope.builtin").find_files({cwd = vim.fn.stdpath('config')}) end, { desc = "Search neovim cache" })
-vim.keymap.set("n", "<leader>sl", function () require("telescope.builtin").find_files({cwd = vim.fn.stdpath('log')}) end, { desc = "Search neovim log" })
-vim.keymap.set("n", "<leader>sd", function () require("telescope.builtin").find_files({cwd = vim.fn.stdpath('data')}) end, { desc = "Search neovim data " })
-vim.keymap.set("n", "<leader>sm", function () require("telescope.builtin").find_files({cwd = vim.fn.stdpath('cache')}) end, { desc = "Search neovim cache " })
-
 return {
   "nvim-telescope/telescope.nvim",
   cmd = "Telescope",
   branch = "0.1.x",
+  keys = {
+    {"<leader>f", "<cmd>Telescope find_files<cr>", mode = "n",  desc = "Find files" },
+    {"<leader>/", "<CMD>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", mode = "n", desc = "Search for cwd"},
+    {"<leader>'", "<cmd>Telescope resume<cr>", mode = "n",  desc = 'Search resume' },
+    {"<leader>:", "<cmd>Telescope command_history<cr>", mode = "n",  desc = 'Search command history' },
+    {"<leader>sp", "<cmd>Telescope builtin<cr>", mode = "n",  desc = "Search telescope picker" },
+    {"<leader>b", "<cmd>Telescope git_branches<cr>", mode = "n",  desc = "Search branches" },
+    {"<leader>se", function () require("telescope.builtin").symbols({sources = {"emoji", "gitmoji"}}) end, mode = "n",  desc = "Search emoji" },
+    {"<leader>sn", "<cmd>Telescope notify<cr>", mode = "n",  desc = "Search notification" },
+    {"<leader>.", "<cmd>Telescope oldfiles<cr>", mode = "n",  desc = 'Search old files ("." for repeat)' },
+    {"<leader>k", "<cmd>Telescope help_tags<cr>", mode = "n",  desc = 'Search help tags' },
+    {"<leader>c", "<cmd>Telescope git_bcommits<cr>", mode = "n",  desc = 'Search commits for current buffer' },
+    {"<leader>C", "<cmd>Telescope git_commits<cr>", mode = "n",  desc = 'Search commits' },
+    {"<leader>g", function() require('telescope.builtin').git_status({path_display = {'tail'}}) end, mode = "n",  desc = 'Search edited / added files' },
+
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-symbols.nvim",
     "nvim-telescope/telescope-live-grep-args.nvim" ,
-    { "nvim-telescope/telescope-ui-select.nvim" },
-    { "nvim-tree/nvim-web-devicons" },
-    {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build =
-        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
-    },
+     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("telescope").setup(
-      {
+    require("telescope").setup( {
         defaults = {
-          -- https://www.reddit.com/r/neovim/comments/r22xrq/in_nvimtelescope_how_can_i_make_the_display_a/
-          -- Uncomment if you want vertical layout (It is probably more convenient on small screens)
-          -- layout_strategy = "vertical",
-          -- layout_config = {
-          --   height = 0.95,
-          --   -- mirror = true,
-          --   preview_cutoff = 0,
-          --   prompt_position = "bottom",
-          -- },
           file_ignore_patterns = { "%.png", "%.pdf", },
           mappings = {
             i = {
@@ -59,25 +38,6 @@ return {
           },
 
         },
-
-        pickers = {
-          colorscheme = {
-            enable_preview = true,
-          },
-        },
-
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown(),
-          },
-        },
       })
-
-    local is_success, _ = pcall(require('telescope').load_extension, 'fzf')
-    if not is_success then
-      print("Telescope native is not installed")
-    end
-
-    require("telescope").load_extension("ui-select")
   end,
 }
