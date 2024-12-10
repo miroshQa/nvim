fdc914bbdb348054e2448260553b9e04c64ff23b
Mirion <mironnesvetaylov@gmail.com>
2024-12-10 22:59:36 +0300

    Remove dap-ui. Fix ctrl + w behaviour in dap repl prompt

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,4 +1,11 @@
-
+-- https://github.com/mfussenegger/nvim-dap/issues/786
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("PromptBufferCtrlwFix", {}),
+  pattern = {"dap-repl"},
+  callback = function()
+    vim.keymap.set("i", "<C-w>", function() vim.cmd("normal! bcw") end, {buffer = true})
+  end
+})
 
 
 return {
@@ -10,23 +17,20 @@ return {
     "nvim-neotest/nvim-nio",
     "theHamsta/nvim-dap-virtual-text",
     "jay-babu/mason-nvim-dap.nvim",
-    "rcarriga/nvim-dap-ui",
     "nvim-telescope/telescope-dap.nvim",
   },
   keys = {
-    {"<M-d>", "<cmd>DapStepOver<CR>", "Step over"},
-    {"<M-s>",  "<cmd>DapStepOut<CR>", "Step out"},
-    {"<M-f>",   "<cmd>DapStepInto<CR>", "Step into"},
-    {"<leader>dc", "<cmd>DapStepContinue<CR>", "Continue"},
-    {"<leader>du", function() require('dap') require('dapui').toggle() end, "UI toggle"},
+    {"<leader>do", "<cmd>DapStepOver<CR>", "Step over"},
+    {"<leader>dO",  "<cmd>DapStepOut<CR>", "Step out"},
+    {"<leader>di",   "<cmd>DapStepInto<CR>", "Step into"},
+    {"<leader>dr",   "<cmd>DapToggleRepl<CR>", "Toggle repl"},
+    {"<leader>dc", "<cmd>DapContinue<CR>", "Continue"},
     {"<leader>dt", "<cmd>DapTerminate<CR>", "Termniate"},
     {"<leader>dh", function() require('dap').run_to_cursor() end, "Debug HERE (Run to cursoer)"},
     {"<leader>db", function() require('dap').list_breakpoints() end, "Breakpoinst list"},
     {"<leader>dB", function() require('dap').clear_breakpoints() end, "Clear breakpoints list"},
     {"<leader>df", function() require('dap').focus_frame() end, "Go to paused"},
     {"<leader>db", function() require('dap').toggle_breakpoint() end, "Toggle breakpont"},
-    {"<leader>di", function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, "Set conditional breakpoint ([i]f)"},
-    {"<leader>de", function() require('dap').set_exception_breakpoints() end, "Set exception breakpont"},
     {"<leader>ds", function() require('dap.ui.widgets').centered_float(require('dap.ui.widgets').scopes) end, "Scopes"},
     {"[s", function() require('dap').down() end, "Stack frame down"},
     {"]s", function() require('dap').up() end, "Stack frame up"},
@@ -45,8 +49,6 @@ return {
       },
       handlers = {
         function(config)
-          -- all sources with no handler get passed here
-          -- Keep original functionality
           require('mason-nvim-dap').default_setup(config)
         end,
 
@@ -56,15 +58,11 @@ return {
     require("telescope").load_extension("dap")
     -- Interface Setup
     require("nvim-dap-virtual-text").setup({ virt_text_pos = "eol"})
-    local dapui = require("dapui")
     vim.fn.sign_define('DapBreakpoint',          { text='', texthl='red'})
     vim.fn.sign_define('DapBreakpointCondition', { text='', texthl='blue'})
     vim.fn.sign_define('DapBreakpointRejected',  { text='', texthl='orange'})
     vim.fn.sign_define('DapStopped',             { text='󰁕', texthl='green'})
     vim.fn.sign_define('DapLogPoint',            { text='.>', texthl='yellow', linehl='DapBreakpoint', numhl='DapBreakpoint' })
-
-    dapui.setup()
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
 
     vim.api.nvim_command 'autocmd FileType dap-float nnoremap <buffer><silent> q <cmd>close!<CR>'
   end,
