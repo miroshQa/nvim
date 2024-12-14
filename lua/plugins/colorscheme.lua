a315f23f5de93b802c60f499d63e93dd2f6cbff4
Mirion <mironnesvetaylov@gmail.com>
2024-12-14 03:07:01 +0300

    change some neo-tree highlight groups

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,12 +4,15 @@ return {
   config = function()
     vim.g.gruvbox_baby_use_original_palette = true
     vim.g.gruvbox_baby_color_overrides = {}
-
-    -- See help IncSearch / CurSearch
+    local c = require("gruvbox-baby.colors")
 
     vim.g.gruvbox_baby_highlights = {
       IncSearch = { bg = "#ff9e64", fg = "White" },
       CurSearch = { link = "IncSearch" },
+      NeoTreeNormal = { fg = c.foreground, bg = c.background },
+      NeoTreeNormalNC = { fg = c.foreground, bg = c.background },
+      FloatBorder = { bg = c.background },
+      NormalFloat = { fg = c.foreground, bg = c.background },
     }
     vim.cmd.colorscheme("gruvbox-baby")
   end,
