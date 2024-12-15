09043699bab8cd1a4c7795d14e9fe46dfa528ba6
Mirion <mironnesvetaylov@gmail.com>
2024-12-15 23:34:21 +0300

    Change some dap keymaps

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,7 +7,6 @@ vim.api.nvim_create_autocmd("FileType", {
   end
 })
 
-
 return {
   "mfussenegger/nvim-dap",
   lazy = true,
@@ -18,24 +17,24 @@ return {
     "theHamsta/nvim-dap-virtual-text",
     "jay-babu/mason-nvim-dap.nvim",
     "nvim-telescope/telescope-dap.nvim",
-    {"rcarriga/nvim-dap-ui", opts = {}}
   },
   keys = {
-    {"<leader>do", "<cmd>DapStepOver<CR>", "Step over"},
-    {"<leader>dO", "<cmd>DapStepOut<CR>", "Step out"},
-    {"<leader>di", "<cmd>DapStepInto<CR>", "Step into"},
-    {"<leader>dr", "<cmd>Telescope dap configurations<CR>", "Toggle repl"},
-    {"<leader>dc", "<cmd>DapContinue<CR>", "Continue"},
+
+    {"<C-n>", "<cmd>DapStepOver<CR>", "Debug: step over (next line)"},
+    {"<C-p>", "<cmd>DapStepOut<CR>", "Debug: step out (prev func call)"},
+    {"<C-g>", "<cmd>DapStepInto<CR>", "Debug: step into (Go deeper)"},
+    {"<C-c>", "<cmd>DapContinue<CR>", "Debug: continue"},
+
+    {"<leader>b", "<cmd>DapToggleBreakpoint<CR>", "Debug: toggle breakpoint"},
+    { '<leader>B', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Debug: Set Breakpoint', },
+    {"<leader>r", function() require('dap').run_to_cursor() end, "Debug: run to cursor"},
+
     {"<leader>dt", "<cmd>DapTerminate<CR>", "Termniate"},
-    {"<leader>dh", function() require('dap').run_to_cursor() end, "Debug HERE (Run to cursoer)"},
+
     {"<leader>df", "<cmd>Telescope dap frames<CR>", "Go to paused"},
-    {"<leader>du", function() require("dapui").toggle() end, "Go to paused"},
-    {"<leader>dF", function() require('dap').focus_frame() end, "Go to paused"},
-    {"<leader>db", function() require('dap').toggle_breakpoint() end, "Toggle breakpont"},
-    {"<leader>dB", "<cmd>Telescope dap list_breakpoints<CR>", "Toggle breakpont"},
+
+    {"<leader>dB", "<cmd>Telescope dap list_breakpoints<CR>", "Telescope breakponts"},
     {"<leader>ds", function() require('dap.ui.widgets').centered_float(require('dap.ui.widgets').scopes) end, "Scopes"},
-    {"[s", function() require('dap').down() end, "Stack frame down"},
-    {"]s", function() require('dap').up() end, "Stack frame up"},
   },
   config = function()
     require("mason").setup() -- Function vim.fn.exepath will not work if we do not setup mason first
@@ -67,12 +66,6 @@ return {
     }
 
     dap.defaults.fallback.force_external_terminal = true
-
-    local dapui = require("dapui")
-    dap.listeners.before.attach.dapui_config = function() dapui.open() end
-    dap.listeners.before.launch.dapui_config = function() dapui.open() end
-    dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
-    dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
 
     vim.fn.sign_define('DapBreakpoint',          { text='', texthl='red'})
     vim.fn.sign_define('DapBreakpointCondition', { text='', texthl='blue'})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     {"<leader><leader>", "<cmd>Telescope buffers<cr>", mode = "n",  desc = "Find buffers" },
     {"<leader>/", "<CMD>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", mode = "n", desc = "Search for cwd"},
     {"<leader>'", "<cmd>Telescope resume<cr>", mode = "n",  desc = 'Search resume' },
-    {"<leader>b", "<cmd>Telescope git_branches<cr>", mode = "n",  desc = "Search branches" },
+    {"<leader>sb", "<cmd>Telescope git_branches<cr>", mode = "n",  desc = "Search branches" },
     {"<leader>se", function () require("telescope.builtin").symbols({sources = {"emoji", "gitmoji"}}) end, mode = "n",  desc = "Search emoji" },
     {"<leader>.", "<cmd>Telescope oldfiles<cr>", mode = "n",  desc = 'Search old files ("." for repeat)' },
     {"<leader>k", "<cmd>Telescope help_tags<cr>", mode = "n",  desc = 'Search help tags' },
@@ -13,7 +13,6 @@ return {
     {"<leader>C", "<cmd>Telescope git_commits<cr>", mode = "n",  desc = 'Search commits' },
     {"<leader>g", "<cmd>silent! Telescope git_status<CR>", mode = "n",  desc = 'Search edited / added files' },
     {"<leader>i", "<cmd>Telescope diagnostics<CR>", mode = "n", desc = "Search code [I]nfo (code diagnostics)"},
-    {"<leader>m", "<cmd>Telescope man_pages<CR>", mode = "n", desc = "Search man pages"},
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
