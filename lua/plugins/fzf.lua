9ec005fd60927c1e3491d9a256507bb4ca69a513
Mirion <mironnesvetaylov@gmail.com>
2025-01-15 13:16:54 +0300

    fzf-lua tweaks

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,7 +1,7 @@
 return {
   "ibhagwan/fzf-lua",
+  branch = "hide",
   keys = {
-    -- General
     { "<leader>f", function() require('fzf-lua').files() end },
     { "<leader>'", function() require("fzf-lua").resume() end },
     { "<leader>k", function() require("fzf-lua").help_tags() end },
@@ -13,6 +13,8 @@ return {
     -- LSP
     { "<leader>s", function() require("fzf-lua").lsp_document_symbols() end,       mode = "n" },
     { "<leader>S", function() require("fzf-lua").lsp_live_workspace_symbols() end, mode = "n" },
+    { "<leader>i", function() require("fzf-lua").lsp_document_diagnostics() end, mode = "n" },
+    { "<leader>I", function() require("fzf-lua").lsp_workspace_diagnostics() end, mode = "n" },
     { "gd",        function() require("fzf-lua").lsp_definitions() end,            mode = "n" },
     { "gr",        function() require("fzf-lua").lsp_references() end,             mode = "n" },
     { "go",        function() require("fzf-lua").lsp_code_actions() end,           mode = { "v", "n" } },
@@ -20,32 +22,23 @@ return {
   },
   config = function()
     require("fzf-lua").setup({
-      actions = {
-        -- https://github.com/ibhagwan/fzf-lua/issues/1595
-        files = {
-          ['enter'] = {
-            fn = function(sel, o)
-              require('fzf-lua').hide()
-              require('fzf-lua.actions').file_edit_or_qf(sel, o)
-            end,
-            exec_silent = true,
-          },
-        },
-      },
+      "hide",
       files = {
         git_icons = false,
       },
       winopts = {
-        width = 0.90,
+        row = 0.5,
+        width = 0.8,
+        height = 0.8,
         preview = {
           horizontal = 'right:50%',
+          scrollbar = false,
         },
         backdrop = 100,
       },
       keymap = {
         builtin = {
-          true, -- inherit all other default binds
-          ["<Esc>"] = "hide",
+          true,
           ["<C-d>"] = "preview-page-down",
           ["<C-u>"] = "preview-page-up",
         }
