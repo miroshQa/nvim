72498b3dbc59249699927fb6711ff0625ff45d85
Mirion <mironnesvetaylov@gmail.com>
2025-02-21 11:43:40 +0300

    qtile bar tweaks

--- de/qtile/screens.py
+++ de/qtile/screens.py
@@ -1,15 +1,15 @@
-from libqtile import hook
-from libqtile import bar, layout, qtile, widget
+from libqtile import bar, layout, qtile
+from qtile_extras import widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
-from libqtile.lazy import lazy
-from qtile_extras.widget.decorations import RectDecoration
+from qtile_extras.widget.bluetooth import Bluetooth
+from qtile_extras.widget.decorations import RectDecoration, BorderDecoration
 from colors import colors
 from utils import *
 
 
 widget_radius = 18
-font_size = 48
-wallpaper = "~/.config/wallpapers/secluded-grove-pixel.png"
+fontsize = 50
+wallpaper = "~/.config/wallpapers/ancientcastle.jpg"
 
 sep_config = {
         "size_percent": 0,
@@ -26,32 +26,67 @@ screens = [
                     active=colors["foreground"],
                     inactive=colors["color8"],
                     disable_drag=True,
-                    icon_size=300,
+                    fontsize=fontsize,
                     radius=True,
                     padding_x=0,
-                    margin_x=28,
+                    margin_x=50,
+                    decorations=[
+                        RectDecoration(
+                            colour=colors["background"],
+                            radius=widget_radius,
+                            filled=True,
+                            padding_y=0
+                        )
+                    ]
                 ),
                 widget.Spacer(),
                 widget.Clock(
                     format="󰥔 %I:%M",
                     foreground=colors["color4"],
+                    fontsize=fontsize,
+                    decorations=[
+                        RectDecoration(
+                            colour=colors["background"],
+                            radius=widget_radius,
+                            filled=True,
+                            padding_y=0
+                        )
+                    ], 
                 ),
+                widget.Sep(**sep_config),
                 widget.Memory(
+                    fontsize=fontsize,
                     format="󰍛 {MemUsed:.0f}{mm}",
                     foreground=colors["color5"],
+
+                    decorations=[
+                        RectDecoration(
+                            colour=colors["background"],
+                            radius=widget_radius,
+                            filled=True,
+                            padding_y=0
+                        )
+                    ]
                 ),
                 widget.Sep(**sep_config),
                 widget.CPU(
+                    fontsize=fontsize,
                     format="󰘚 {load_percent}%",
                     foreground=colors["color6"],
+
+                    decorations=[
+                        RectDecoration(
+                            colour=colors["background"],
+                            radius=widget_radius,
+                            filled=True,
+                            padding_y=0
+                        )
+                    ]
                 ),
-                widget.Volume(
-                    mute_format='', 
-                    unmute_format='  {volume}%', 
-                    step=5,
-                    foreground=colors["color6"],
-                ),
+                widget.Sep(**sep_config),
+                widget.Sep(**sep_config),
                 widget.Battery(
+                    fontsize=fontsize,
                     format="{char} {percent:2.0%}",
                     charge_char="󰂄",
                     discharge_char="󰁹",
@@ -60,19 +95,51 @@ screens = [
                     show_short_text=False,
                     not_charging_char="󰁹",
                     foreground=colors["color2"],
+
+                    decorations=[
+                        RectDecoration(
+                            colour=colors["background"],
+                            radius=widget_radius,
+                            filled=True,
+                            padding_y=0
+                        )
+                    ]
                 ),
+                widget.Sep(**sep_config),
                 widget.Backlight(
+                    fontsize=fontsize,
                     fmt="󰃚 {}",
                     backlight_name=get_backlight_device(),
                     foreground=colors["color3"],
+
+                    decorations=[
+                        RectDecoration(
+                            colour=colors["background"],
+                            radius=widget_radius,
+                            filled=True,
+                            padding_y=0
+                        )
+                    ]
                 ),
-                widget.KeyboardLayout(configured_keyboards=['us','ru']),
+                widget.Sep(**sep_config),
+                widget.KeyboardLayout(
+                    fontsize=fontsize,
+                    configured_keyboards=['us','ru'],
+                    decorations=[
+                        RectDecoration(
+                            colour=colors["background"],
+                            radius=widget_radius,
+                            filled=True,
+                            padding_y=0
+                        )
+                    ]
+                ),
             ],
-            font_size,
+            fontsize,
             opacity = 1,
-            # background="#00000000",
+            background="#00000000",
             # border_width=[0, 0, 0, 0],
-            # margin=[0, 0, 0, 0],
+            margin=[10, 10, 10, 10],
             # border_color=["000000", "000000", "000000", "000000"],
         ),
         x11_drag_polling_rate = 120,
